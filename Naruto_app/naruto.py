6efb28826029e14ac4be4c7704ef6b8d3857a5d9
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-14 19:35:43 -0500

    Update naruto.py

--- Naruto_app/naruto.py
+++ Naruto_app/naruto.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+import requests
+import PIL
+
+url = "https://narutodb.xyz/api/character?page=1&limit=1431"
+
+response = requests.get(url).json()
+
+df = pd.DataFrame(response["characters"])
+df.drop(columns=df.columns[12:], inplace=True)
+
+st.sidebar.title("Naruto Universe")
 
