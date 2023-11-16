9eda853f1e0f71e6232752ddef07fe4a723d50c5
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-15 19:38:06 -0500

    Update naruto.py

--- Naruto_app/naruto.py
+++ Naruto_app/naruto.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import numpy as np
+import plotly.express as px
 import streamlit as st
 import requests
 from PIL import Image
 
+
 url = "https://narutodb.xyz/api/character?page=1&limit=1431"
 
+#Dataframe:
 response = requests.get(url).json()
-
 df = pd.DataFrame(response["characters"])
 df.drop(columns=df.columns[12:], inplace=True)
 
+
 #Sidebar:
-st.sidebar.image(Image.open("Naruto_app/icons8-naruto-512.png"), width=90, clamp=True, caption= "Naruverse App")
-st.sidebar.title("Naruto Universe")
-st.sidebar.markdown("##")
+st.sidebar.image(Image.open("Naruto_app/icons8-naruto-512.png"), width=00, clamp=True, caption= "Naruverse App")
+characters = st.button("🍥Characters")
+villages = st.button("🏛️Villages")
+clans = st.button("🥷🏻Clans")
+tailed_beasts = st.button("🦊Tailed Beasts")
+akatsuki = st.button("🩸Akatsuki")
