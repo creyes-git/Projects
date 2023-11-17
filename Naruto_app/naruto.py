4460ed58c70aa197a2316d42a30525b346cf21af
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-17 12:37:32 -0500

    Update naruto.py

--- Naruto_app/naruto.py
+++ Naruto_app/naruto.py
@@ -19,7 +19,7 @@ def landing_page():
     c1, c2 , c3, c4, c5 = st.columns(5)
     c1.image(Image.open("Naruto_app/wp2725528-naruto-kakashi-wallpaper.png"), width=200, clamp=True)
     c3.image(Image.open("Naruto_app/Landing Page.png"), width=200, clamp=True)
-    c2.image(Image.open("Naruto_app/wp4981538-naruto-minimal-wallpapers.png"), width=200, clamp=True)
+    c2.image(Image.open("Naruto_app/wp10367350-minato-minimalist-wallpapers.jpg"), width=200, clamp=True)
     c5.image(Image.open("Naruto_app/wp10926937-minimalist-4k-naruto-wallpapers.png"), width=200, clamp=True)
     c4.image(Image.open("Naruto_app/wp10926937-minimalist-4k-naruto-wallpapers.png"), width=200, clamp=True)
     st.title("About Naruto")
