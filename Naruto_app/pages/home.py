6cf5df2ffe41c08efeb8e66842b8bc27fec9e550
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-18 13:07:54 -0500

    Update home.py

--- Naruto_app/pages/home.py
+++ Naruto_app/pages/home.py
@@ -2,7 +2,9 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Naruverse", page_icon="🍥")
+menu = ["Home", "Characters", "Tailed Beasts", "Akatsuki", "Villages", "Clans"]
+
+st.set_page_config(page_title="Naruverse", page_icon="🍥", layout="wide", menu_items= menu)
 
 #Introduction
 c1, c2 , c3, c4, c5 = st.columns(5)
