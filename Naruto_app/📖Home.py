ebfa4b301d15c6d37cef99dd6a0679af398c130a
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-18 14:20:26 -0500

    Update 📖Home.py

--- Naruto_app/📖Home.py
+++ Naruto_app/📖Home.py
@@ -4,6 +4,9 @@ from PIL import Image
 
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
+st.sidebar.image(Image.open("Naruto_app/images/icons8-naruto-512.png"), width=90, clamp=True, caption= "Naruverse")
+
+
 #Introduction
 c1, c2 , c3, c4, c5 = st.columns(5)
 c1.image(Image.open("Naruto_app/images/wp2725528-naruto-kakashi-wallpaper.png"), width=150, clamp=True)
@@ -23,11 +26,3 @@ to enjoy a testament to the series’
 enduring influence and popularityaround the world.''')                
 
 
-'''#Sidebar:
-st.sidebar.image(Image.open("images\\icons8-naruto-512.png"), width=90, clamp=True, caption= "Naruverse")
-introduction = st.sidebar.button("📖Introduction")
-tailed_beasts = st.sidebar.button("🦊Tailed Beasts")
-characters = st.sidebar.button("🍥Characters")
-akatsuki = st.sidebar.button("🩸Akatsuki")
-villages = st.sidebar.button("🏛️Villages")
-clans = st.sidebar.button("🥷🏻Clans")'''
