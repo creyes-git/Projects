af7e7537dd55a5a6e45a96446a1383481f1766bd
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-18 13:43:51 -0500

    Update home.py

--- Naruto_app/home.py
+++ Naruto_app/home.py
@@ -23,11 +23,11 @@ to enjoy a testament to the series’
 enduring influence and popularityaround the world.''')                
 
 
-#Sidebar:
+'''#Sidebar:
 st.sidebar.image(Image.open("images\\icons8-naruto-512.png"), width=90, clamp=True, caption= "Naruverse")
 introduction = st.sidebar.button("📖Introduction")
 tailed_beasts = st.sidebar.button("🦊Tailed Beasts")
 characters = st.sidebar.button("🍥Characters")
 akatsuki = st.sidebar.button("🩸Akatsuki")
 villages = st.sidebar.button("🏛️Villages")
-clans = st.sidebar.button("🥷🏻Clans")
+clans = st.sidebar.button("🥷🏻Clans")'''
