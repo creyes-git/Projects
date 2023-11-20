8f66265c3cbb63182a4be8b72e53166549e67248
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-20 18:06:44 -0500

    Update 📖Home.py

--- Naruto_app/📖Home.py
+++ Naruto_app/📖Home.py
@@ -11,15 +11,14 @@ c2.image(Image.open("Naruto_app/images/wp10367350-minato-minimalist-wallpapers.j
 c3.image(Image.open("Naruto_app/images/Landing Page.png"), width=150, clamp=True)
 c4.image(Image.open("Naruto_app/images/wp10926937-minimalist-4k-naruto-wallpapers.png"), width=150, clamp=True)
 c5.image(Image.open("Naruto_app/images/wp5129198-anime-lambang-uciha-wallpapers.jpg"), width=150, clamp=True)
+
 st.title("About Naruto")
-st.text('''NARUTO is a manga series created by MasashiKishimoto 
-that was serialized in Weekly ShonenJump. Met with popularity 
-followingits initial publication in 1999, the series went on to receive 
-an anime adaptation that commenced broadcasting in 2002. The manga 
-concluded in 2014 with a total of 700 chapters. The total number of print 
-copies sold globally exceeds 250 million, and even now NARUTO events, 
-games, merchandise,and more continue to be created and released for fans 
-to enjoy a testament to the series’ 
-enduring influence and popularityaround the world.''')                
+
+st.markdown('''NARUTO is a manga series created by Masashi Kishimoto that was serialized in Weekly ShonenJump. 
+Met with popularity followingits initial publication in 1999, the series went on to receive an anime adaptation
+that commenced broadcasting in 2002. The manga concluded in 2014 with a total of 700 chapters. The total number 
+of print copies sold globally exceeds 250 million, and even now NARUTO events, games, merchandise,and more 
+continue to be created and released for fans to enjoy a testament to the series’ enduring influence and popularityaround the world.''')                
+
 
 
