733e88b7c3a06b2676e531dc22ab22f17c0191c3
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-20 19:56:53 -0500

    Update 🌎World.py

--- Naruto_app/pages/🌎World.py
+++ Naruto_app/pages/🌎World.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="World Building", page_icon="🌎")
+st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
 st.title("Naruto World Construction")
 st.markdown('''The Naruto series is set on a fictional terrestrial blue planet called Earth. Most of the series takes place on a large 
@@ -15,3 +15,6 @@ for the entire country. Hidden Village heads are the generals that take care of 
 Japan in many aspects; those countries maintain balance between themselves through nothing but power. Treaties are periodically signed, 
 but they are generally not worth much more than the paper they are written on.''')
 st.image(Image.open("Naruto_app//images//Naruto_World_Map.png"), use_column_width= True, clamp=True)
+
+st.image(Image.open("Naruto_app//images//Land_of_Earth_Symbol.png"),caption="The Land of Earth")
+st.markdown("The Land of Earth (土の国, Tsuchi no Kuni) has seen little attention in the series thus far. It is located north-west of the Land of Fire. Its government leader is the Earth Daimyō. The country is mostly comprised of desolate, rocky areas. The border of the Land of Earth runs along a rocky mountain range, blocking communication with other countries. The wind blowing from the north passes over these mountains, carrying small rocks from the Land of Earth to the surrounding countries. This famous natural phenomenon is called Rock Rain (岩雨, Gan'u).")
