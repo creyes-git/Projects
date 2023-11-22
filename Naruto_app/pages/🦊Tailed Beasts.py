da1e2bf92cf0a5ad2b33464b9371b4d86173d84b
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-21 19:26:09 -0500

    Update 🦊Tailed Beasts.py

--- Naruto_app/pages/🦊Tailed Beasts.py
+++ Naruto_app/pages/🦊Tailed Beasts.py
@@ -2,4 +2,6 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Naruverse", page_icon="🍥", initial_sidebar_state= "expanded")
+st.set_page_config(page_title="Naruverse", page_icon="🍥")
+
+st.image(Image.open("Naruto_app/images/all beasts.png"), width=150, clamp=True)
