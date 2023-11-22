755ba22c835b6d838503e57729616c834bb78f68
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-21 19:27:51 -0500

    Update 🦊Tailed Beasts.py

--- Naruto_app/pages/🦊Tailed Beasts.py
+++ Naruto_app/pages/🦊Tailed Beasts.py
@@ -4,4 +4,4 @@ from PIL import Image
 
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
-st.image(Image.open("Naruto_app/images/all beasts.png"), width=150, clamp=True)
+st.image(Image.open("Naruto_app/images/all beasts.png"), use_column_width= True, clamp=True)
