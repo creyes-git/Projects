196fdaa8d00c8540931a9844f4cafd712c5ed5f2
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-22 18:00:11 -0500

    Update 🦊Tailed Beasts.py

--- Naruto_app/pages/🦊Tailed Beasts.py
+++ Naruto_app/pages/🦊Tailed Beasts.py
@@ -1,36 +1,99 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
-
+st.title("Tailed Beasts History and Characteristics")
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
 st.image(Image.open("Naruto_app/images/all beasts.png"), use_column_width= True, clamp=True)
-st.title("Tailed Beasts")
+
 
 st.image(Image.open("Naruto_app/images/1 cola.png"), use_column_width= 100, caption= "Shukaku")
-st.markdown("")
+st.title("Background")
+st.markdown("Shukaku and the tailed beasts first came into being in the waning days of Hagoromo Ōtsutsuki, who used his Creation of All Things Technique to divide the Ten-Tails' chakra into nine separate bodies and giving each a name. Some time after being created, the Sage explained to the young tailed beasts that a link existed between them despite their separation. He also foretold of a day when they would be brought together again - though not as they were - and that at that time someone would emerge to show them what true power is. In the anime, before his death, Hagoromo sent Shukaku to live in a temple built for the purpose of protecting it in a desert region that would later become part of the Land of Wind.")
+st.title("Personality")
+st.markdown("Shukaku is characterised as a childish and short-tempered individual, and often speaks in a bombastic and incoherent manner, giving it the demeanour of a drunkard. It uses ore-sama (オレ様, literally meaning: my esteemed self) when referring to itself, as it expresses great pride in its own abilities, especially its sealing techniques and defences. According to Gaara, prior to its introduction, Shukaku relishes the opportunity to kill anyone in sight, even more so during a full moon. Shukaku has also shown to harbour a bitter grudge towards Kurama due to the fox's beliefs that the tailed beasts' strength is measured by the number of tails, therefore viewing the tanuki as the weakest. Because of this, Shukaku strives to find any opportunity to surpass its arrogant rival.")
+st.title("Abilities")
+st.markdown("As a tailed beast, Shukaku possesses a large quantity of chakra and can perform the Tailed Beast Ball. As a precaution, if its jinchūriki lost control and fully transformed into Shukaku inside of Suna, the villagers were to either evacuate beforehand or seek shelter due to the risk of mass casualty from its rampage. Shukaku has the ability to control its jinchūriki whenever they sleep, leading its host to develop insomnia. Shukaku has great physical strength while using its tail, as seen when overlapping it with the other tailed beasts to shatter and even obliterate a senjutsu-enhanced humanoid Susanoo. Shukaku's power is quite impressive to the likes of the Ōtsutsuki Clan, with Urashiki Ōtsutsuki expressing glee in taking it.")
+st.markdown('''Sand Manipulation: Shukaku enforcing a fūinjutsu with its curse seal markings.
+Shukaku's most notable ability is its sand manipulation, using it as an effective weapon and shield. Since its whole body is completely made of sand, the tanuki can ignore most damage that would otherwise be detrimental, as shown when it had one of its arms chopped off by Gamabunta and was still able to keep on fighting. Taking pride in its motto of absolute defence, Shukaku's sand is durable enough to block a senjutsu-enhanced Susanoo sword with ease. It is capable of performing fūinjutsu by using its natural cursed seal markings across its own sand body, which gives off a magnetic field to bind its targets. One of its techniques, the Desert Layered Imperial Funeral Seal, is strong enough to temporarily restrain and withstand powerful techniques, like the Susanoo.
+Nature Transformation: Shukaku can use Wind and Earth Release nature transformations. It can rapidly fire wind bullets with enough force to level entire forests. Likewise, it can combine its wind abilities with Gaara's sand in order to produce a mass of fast-moving sand bullets. He could also charge these wind bullets to the point they could fire like a Tailed Beast Ball, and wipe out entire landscapes.[28] Shukaku can combine wind and earth elements to use Magnet Release, and is the only tailed beast which can use cursed seals from its markings and infuse them into the Rasengan to create the Sage Art: Magnet Release Rasengan, which was powerful enough to restrain Madara Uchiha's shadow when he was the Ten-Tails' jinchūriki.''')
+
 
 st.image(Image.open("Naruto_app/images/2 cola.png"), use_column_width= 100, caption= "Matatabi")
-st.write("Skills")
+st.title("Background")
 st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
+st.markdown("")
+
+
+
 st.image(Image.open("Naruto_app/images/3 cola.png"), use_column_width= 100, caption= "Isobu")
+st.title("Background")
 st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
+st.markdown("")
+
+
 
 st.image(Image.open("Naruto_app/images/4 cola.png"), use_column_width= 100, caption= "Son Gokū")
+st.title("Background")
 st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
+st.markdown("")
+
+
 
 st.image(Image.open("Naruto_app/images/5 cola.png"), use_column_width= 100, caption= "Kokuō")
+st.title("Background")
 st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
+st.markdown("")
+
+
 
 st.image(Image.open("Naruto_app/images/6 cola.png"), use_column_width= 100, caption= "Saiken")
+st.title("Background")
+st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
 st.markdown("")
 
-st.image(Image.open("Naruto_app/images/7 cola.png"), use_column_width= 100, caption= "Chōmei")
+
+
+st.image(Image.open("Naruto_app/images/7 cola.webp"), use_column_width= 100, caption= "Chōmei")
+st.title("Background")
 st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
+st.markdown("")
+
+
 
 st.image(Image.open("Naruto_app/images/8 cola.png"), use_column_width= 100, caption= "Gyūki")
+st.title("Background")
+st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
 st.markdown("")
 
-st.image(Image.open("Naruto_app/images/9 cola.png"), use_column_width= 100, caption= "Kurama")
+
+
+st.image(Image.open("Naruto_app/images/9 cola.webp"), use_column_width= 100, caption= "Kurama")
+st.title("Background")
+st.markdown("")
+st.title("Personality")
+st.markdown("")
+st.title("Abilities")
 st.markdown("")
 
