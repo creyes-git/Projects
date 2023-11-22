fed68754ac121f52575c8f64c394ddb9ff73b7c5
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-21 19:58:07 -0500

    Update 🦊Tailed Beasts.py

--- Naruto_app/pages/🦊Tailed Beasts.py
+++ Naruto_app/pages/🦊Tailed Beasts.py
@@ -4,32 +4,33 @@ from PIL import Image
 
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
-st.title("Tailed Beasts")
 st.image(Image.open("Naruto_app/images/all beasts.png"), use_column_width= True, clamp=True)
-
+st.title("Tailed Beasts")
 
 st.image(Image.open("Naruto_app/images/1 cola.png"), use_column_width= 100, caption= "Shukaku")
-
+st.markdown("")
 
 st.image(Image.open("Naruto_app/images/2 cola.png"), use_column_width= 100, caption= "Matatabi")
-
-
+st.write("Skills")
+st.markdown("")
 st.image(Image.open("Naruto_app/images/3 cola.png"), use_column_width= 100, caption= "Isobu")
-
+st.markdown("")
 
 st.image(Image.open("Naruto_app/images/4 cola.png"), use_column_width= 100, caption= "Son Gokū")
-
+st.markdown("")
 
 st.image(Image.open("Naruto_app/images/5 cola.png"), use_column_width= 100, caption= "Kokuō")
-
+st.markdown("")
 
 st.image(Image.open("Naruto_app/images/6 cola.png"), use_column_width= 100, caption= "Saiken")
+st.markdown("")
 
-
-st.image(Image.open("Naruto_app/images/7 cola.webp"), use_column_width= 100, caption= "Chōmei")
-
+st.image(Image.open("Naruto_app/images/7 cola.png"), use_column_width= 100, caption= "Chōmei")
+st.markdown("")
 
 st.image(Image.open("Naruto_app/images/8 cola.png"), use_column_width= 100, caption= "Gyūki")
+st.markdown("")
 
+st.image(Image.open("Naruto_app/images/9 cola.png"), use_column_width= 100, caption= "Kurama")
+st.markdown("")
 
-st.image(Image.open("Naruto_app/images/9 cola.webp"), use_column_width= 100, caption= "Kurama")
