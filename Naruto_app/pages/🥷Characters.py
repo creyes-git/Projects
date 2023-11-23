9eb0d74d54a25545e459ba8de59b717383e756fc
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-22 19:49:19 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -2,4 +2,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Naruverse", page_icon="🍥", initial_sidebar_state= "expanded")
+st.set_page_config(page_title="Naruverse", page_icon="🍥")
+
+st.title("Naruto Characters")
+st.image(Image.open("Naruto_app/images/characters.png"), use_column_width= True, clamp=True)
