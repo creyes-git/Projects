3689b1adf4b13e8bb3abbc548882b0205fcbf6d1
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-22 19:07:47 -0500

    Update 🦊Tailed Beasts.py

--- Naruto_app/pages/🦊Tailed Beasts.py
+++ Naruto_app/pages/🦊Tailed Beasts.py
@@ -31,60 +31,65 @@ st.markdown("As a tailed beast, Matatabi has an enormous supply of chakra that i
 st.title("Isobu")
 st.image(Image.open("Naruto_app/images/3 cola.png"), use_column_width= 100, caption= "Isobu")
 st.title("Personality")
-st.markdown("")
+st.markdown("Isobu speaks in a timid manner, referring to itself as boku (ボク) which is usually used by young boys. Seeing as Yagura had full control of it and befriended it, it's assumed that Isobu isn't as rude or outspoken as some of the other tailed beasts and got along with its jinchūriki.")
 st.title("Abilities")
-st.markdown("")
-
+st.markdown("As a tailed beast, Isobu possesses an enormous amount of chakra that it can transfer to others and can create the Tailed Beast Ball. It also has the ability to create coral and swim at very high speeds. Kirigakure was so confident of its destructive potential that they, under the influence of Madara Uchiha, intended to unleash Isobu in the midst of Konohagakure to destroy the village. In the anime, it could produce a hallucinogenic mist that exploited the victim's insecurities and forced the victim to face them.")
+st.markdown('''Physical Prowess: Isobu can roll into a ball to move about quickly and attack its opponents, making full use of the protrusions on its shell in a manner similar to the Akimichi clans Spiked Human Bullet Tank. Isobu tough skin and shell also grant it additional defence, allowing it to withstand nearly all forms of attack. It can also use any of its three spiked tails to strike its opponents and break through most defences. In the anime, Isobu only known physical weakness is its eye; as tough as its skin and shell are, it cannot harden its eyes and thus seems to keep one closed at all times.''')
+st.markdown('''Ninjutsu: In the anime, Isobu's abilities were greatly elaborated upon. It could create shockwaves to repel attacks and produce large tidal waves around it, enabling it to overwhelm Guren's Crystal Release several times over the course of their battle. When Isobu ate something, thousands of miniature versions of itself would attack what was eaten within its stomach, presumably to speed up the digestion process. It was also shown capable of Space-Time Ninjutsu, materialising an entrance to a separate dimension in which it could hide its presence, until it was ready to return to wherever it chooses.''')
+st.markdown("Nature Transformation: As a turtle, Isobu has an affinity for Water Release. In the anime, it has shown to fire powerful water blasts that are capable of destroying an entire cliff side.")
 
 
 st.title("Son Gokū")
 st.image(Image.open("Naruto_app/images/4 cola.png"), use_column_width= 100, caption= "Son Gokū")
 st.title("Personality")
-st.markdown("")
+st.markdown("Son Gokū is a very prideful tailed beast, as it detests being called by the name Four-Tails and boasts about its lineage. It uses ore (オレ) when referring to itself. It also hated humans, due to the fact that humans had enslaved it, and believes that apes are more intelligent than humans. However, when talking about the Sage of Six Paths, it showed him great respect, being that also had, apparently, a very deep relationship with him, never forgetting his final words. It also seems to have taken a liking to Naruto Uzumaki, as it was moved by his actions for truly trying to save it from being controlled by Tobi and even gave him some of its chakra before being sealed away. He also treats his fellow tailed beasts with much respect, as he addresses them by their name, as seen with Kurama. Son also has a high amount of respect for those who respect the tailed beasts.")
 st.title("Abilities")
-st.markdown("")
-
+st.markdown("As a tailed beast, Son Gokū possesses an enormous supply of chakra and can create the Tailed Beast Ball. Son also seems knowledgeable in taijutsu, likewise showing deceptive nimbleness of its size, utilising powerful and well-timed kicks against its opponents. Son has a great deal of physical strength, being able to lift and toss Gyūki with ease.")
+st.markdown("Ninjutsu: Son has the ability to use Fire and Earth Release simultaneously, allowing it to use Lava Release. With it, it can create volcanoes, and although never used in the storyline, can breathe a stream of green coloured fire.")
 
 
 st.title("Kokuō")
 st.image(Image.open("Naruto_app/images/5 cola.png"), use_column_width= 100, caption= "Kokuō")
 st.title("Personality")
-st.markdown("")
+st.markdown("Kokuō seems to be a quiet and reserved individual. Though it does not speak much, Kokuō uses the rather old-fashioned watakushi (私) when referring to itself, causing it to come across as very polite. It also has pride as a tailed beast, being dismayed at being controlled and being used as Tobi's puppet during the Fourth Shinobi World War. After the end of the war, Kokuō declared that it wants to seclude itself in the forest, which implies it is a pacifistic individual, not wanting confrontations anymore.")
 st.title("Abilities")
-st.markdown("")
-
+st.markdown("As a tailed beast, Kokuō possesses a massive amount of chakra and can use it to create a Tailed Beast Ball. Its willpower was incredibly strong, as it was the only tailed beast who broke free, albeit briefly, of Tobi's control and attempted to attack the masked man. Kokuō also possesses great physical strength and uses its horns in combat, showing enough ramming-force to injure and knock Gyūki a considerable distance away.")
+st.markdown("Nature Transformation: By combining water and fire-natured chakra to create Boil Release, Kokuō is able to increase the temperature of its chakra to its boiling point. This ability forms the basis of what is referred to as steam-based ninjutsu, granting itself, or its jinchūriki, overwhelming physical strength. When in use, this ability causes the jinchūriki to emit copious amounts of steam from their body.")
 
 
 st.title("Saiken")
 st.image(Image.open("Naruto_app/images/6 cola.png"), use_column_width= 100, caption= "Saiken")
 st.title("Personality")
-st.markdown("")
+st.markdown('''Though much has not been seen of Saiken's personality, the giant slug uses ore (オレ) when referring to itself, ore being a boastful way of saying "I". It also speaks with a dialect which may possibly be Ōsakaben in origin. In the anime, Saiken also seemed to be kinder and not as self-serving and malicious as most other tailed beasts, as when Utakata asked for its help to save Hotaru it seemed to willingly comply; and later it simply receded back into Utakata without trying to take him over. However, it was not above killing for self-preservation, as seen when it killed Harusame when he attempted to extract it from its jinchūriki. In the anime, it speaks with a high pitched voice and appears to be very energetic.''')
 st.title("Abilities")
-st.markdown("")
+st.markdown("As a tailed beast, Saiken has a massive amount of chakra that it can freely share with others and can create a Tailed Beast Ball. Saiken has shown the ability to emit corrosive substances that can instantly disintegrate its target on contact in the form of liquid or gas. Saiken is seen having a great deal of tremendous durability, able to withstand being thrown a great distance by Kurama. Saiken possesses an affinity for Water Release. It can expel a sticky, adhesive substance from its mouth, which is capable of trapping its targets. In the anime, it can use Soap Bubble Ninjutsu.")
 
 
 
 st.title("Chōmei")
 st.image(Image.open("Naruto_app/images/7 cola.webp"), use_column_width= 100, caption= "Chōmei")
 st.title("Personality")
-st.markdown("")
+st.markdown("Chōmei seems to have a happy-go-lucky personality — which it seemingly shares with its jinchūriki as shown when it introduced itself to Naruto, referring to itself as Lucky Seven Chōmei (ラッキーセブン重明, Rakkī sebun Chōmei), which might be an allusion to the fact that it has seven tails and the number seven having long been seen as a lucky as well as a happy number. It also had a habit of using the word lucky in conversation, reflecting its personality.")
 st.title("Abilities")
-st.markdown("")
+st.markdown("As a tailed beast, Chōmei possesses extremely large amounts of chakra that it can share with others and can use the Tailed Beast Ball. Using its wings, it can emit blinding powder and has the ability to fly. It also utilises insect-based attacks, such as biting or ramming its horn into the enemy. In the anime, it can also create a Cocoon which could slow down Chakra Absorption Techniques. It has immense durability, able to withstand being slammed into the ground by Kurama.")
 
 
 
 st.title("Gyūki")
 st.image(Image.open("Naruto_app/images/8 cola.png"), use_column_width= 100, caption= "Gyūki")
 st.title("Personality")
-st.markdown("")
+st.markdown("Gyūki is characterised as a tough and serious individual. It uses ore (オレ) when referring to itself and often speaks in a somewhat rude dialect. Gyūki was also shown to have a good analytical ability, as it told Killer B that if he left Ponta and Sabu and ran away from them, Kisame Hoshigaki and his water dome would follow him and they (Sabu and Ponta) would be freed. Although Gyūki mentioned that it and its brethren disliked Kurama for its arrogance and belief that their strength are based on their number of tails, it insisted that was no reason for not helping Naruto Uzumaki to control Kurama's power, hinting a benevolent side. Since then, both Gyūki and B have taken a liking to Naruto.")
 st.title("Abilities")
-st.markdown("")
-
+st.markdown("Gyūki was noted by Kisame and Kurama to be the second most powerful of the nine tailed beasts. Even the Third Raikage, hailed as the greatest shinobi in Kumo's history, formed a special team to subdue and seal it during its various rampages, with the ushi-oni fighting the Third to a stalemate on one occasion. The Fourth Raikage also once forbade B from fully transforming into Gyūki due to its great power. During the Fourth Shinobi World War, Gyūki held its own against two other tailed beasts despite suffering a past injury.")
+st.markdown("Chakra and Physical Prowess: As a tailed beast, Gyūki possesses a massive supply of powerful chakra and can share it with others. It can replenish its high reserves shortly after an entire day's usage. Should the need arise, it can disturb B's chakra with its own to dispel powerful genjutsu. Gyūki's tentacles, if severed, can be used as a medium for its chakra, such as creating a clone of its host, or transferring chakra to said host should the beast be extracted.")
+st.markdown("Ninjutsu: Gyūki can perform the Tailed Beast Ball, being able to fire it normally, consecutively or as a massive wave. It is strong enough to devastate an entire valley, penetrate nine barriers without exploding, and even push the Ten-Tails' own Tailed Beast Ball back intoits body. Unlike most tailed beasts, Gyūki can supercharge it, creating one many times its own size for much greater power. Going with its octopus theme, it can produce ink to blind its enemies or form ink clones to seal them.")
 
 
 st.title("Kurama")
 st.image(Image.open("Naruto_app/images/9 cola.webp"), use_column_width= 100, caption= "Kurama")
 st.title("Personality")
-st.markdown("")
+st.markdown("Kurama was characterised as a snarky and shrewd individual. It used washi (ワシ) when referring to itself, which is generally used by older men. Kurama expressed great pride in its power, believing itself to be the strongest of the tailed beasts due to its belief that their strength is determined by their number of tails, which earned Kurama nothing but disapproval from its brethren, especially Shukaku. The fox would also react out of jealousy whenever Naruto was not using its power.")
 st.title("Abilities")
-st.markdown("")
+st.markdown("Kurama was widely known as the strongest of the nine tailed beasts. Hashirama Senju, who could subdue the fox, viewed it as too powerful and dangerous to be left roaming free. Even with only half its power, Kurama remained strong enough to defeat five other tailed beasts at the same time. After the Fourth Shinobi World War, Kurama's full power was restored once its two halves reunited inside Naruto, allowing it to defeat Toneri's giant golem and Momoshiki Ōtsutsuki's elemental construct. The absolute peak of its power allowed it to even decisively overwhelm Isshiki Ōtsutsuki. Kurama later became able to manifest itself outside of Naruto, allowing the two to fight independently and away from each other.")
+st.markdown('''Chakra and Physical Prowess: Kurama possessed vast and powerful chakra, the most of the nine tailed beasts alongside Gyūki, and was required to be sealed last into the Demonic Statue of the Outer Path to avoid overloading it. It could also accumulate a huge amount of chakra shortly after it has used up most of its own, enough to be passed on to the entire Allied Shinobi Forces and be felt countries away by non-sensors. Due to Naruto's compatibility with Kurama's chakra, any chakra it possessed is limited for its jinchūriki alone, unless it was moulded to fit the recipient's unique chakra signature. When used by others, the chakra became dense enough to be seen as a visible shroud than can further protect the wearer. Using its chakra, Kurama could shroud itself within its Nine-Tails Chakra Mode to increase its power. As a last-ditch ability''')
+st.markdown('''Ninjutsu: As a tailed beast, Kurama could create a Tailed Beast Ball, and was able to fire it as a beam or rapid blast of spheres. Unlike most tailed beasts, Kurama could supercharge it, creating one many times its own size for much greater power, even on wisps of chakra. One of these was powerful enough to counter a collaborated Tailed Beast Ball from five tailed beasts. At full power, Kurama's Tailed Beast Ball could blast Toneri's giant golem through the moon and into orbit.''')
