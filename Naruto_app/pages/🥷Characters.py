3e7ed7213c388c2d2cd288ba7013903683f742c2
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-22 20:16:02 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
-from data import df
-
+from Naruto_app.pages.data import show_character
+from Naruto_app.pages.data import df
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
 st.title("Naruto Characters")
@@ -10,7 +10,4 @@ st.image(Image.open("Naruto_app/images/characters.png"), use_column_width= True,
 
 character = st.selectbox("Chose the character", df["name"].unique())
 
-def show_character(character):
-    df= df[df["name"] == character]
-    
-    return st.write(df)
+
