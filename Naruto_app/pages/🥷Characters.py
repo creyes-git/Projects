3f80b4f1633ec192b2586d0ad22d66ebe9ecc5d8
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-23 12:02:22 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -5,9 +5,6 @@ from PIL import Image
 
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
-st.title("Naruto Universe Characters")
-st.image(Image.open("Naruto_app/images/characters.jpg"), use_column_width= True, clamp=True)
-
 url = "https://narutodb.xyz/api/character?page=1&limit=1431"
 
 response = requests.get(url).json()
@@ -15,14 +12,22 @@ response = requests.get(url).json()
 df = pd.DataFrame(response["characters"])
 df.drop(columns=df.columns[12:], inplace=True)
 
-character = st.selectbox("Chose the character", df["name"].unique())
 
-def show_character(character):
+
+
+st.title("Naruto Characters")
+st.image(Image.open("Naruto_app/images/characters.png"), use_column_width= True, clamp=True)
+st.warning("Choose the character you want to know more about")
+
+character = st.selectbox("Choose the character", df["name"].unique())
+
+st.write(df[df["name"] == character])
+
+
+
+'''def show_character(character):
     df = df[df["name"] == character]
     
     return st.write(df)
 
-show_character(character)
-
-
-
+show_character(character)'''
