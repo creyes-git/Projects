97653022825eacb315eb2d8e023976135ed15f7f
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-22 20:13:06 -0500

    Update data.py

--- Naruto_app/data.py
+++ Naruto_app/data.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import requests
 
-st.set_page_config(page_title="data", page_icon="🏛")
 url = "https://narutodb.xyz/api/character?page=1&limit=1431"
 
 #Dataframe:
 response = requests.get(url).json()
 df = pd.DataFrame(response["characters"])
 df.drop(columns=df.columns[12:], inplace=True)
+
+def show_character(character):
+    df= df[df["name"] == character]
+    
+    return st.write(df)
