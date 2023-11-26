9e8635099900626cb00a584e25b49524da9d5deb
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-26 13:09:02 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -95,7 +95,7 @@ for i in picked["jutsu"].values[0]:
 c2.markdown("Debut: ")
 debut = str(str(picked["debut"].values[0]).split(",")[0]).split(":")[1]
 debut = debut.replace("'","")
-c2.markdown("Manga: "+ debut.split("")[1:])
+c2.markdown("Manga: "+ debut.split("o")[1])
 
 c2.header("Family: ")
 family = str(picked["family"].values[0]).replace("{","")
