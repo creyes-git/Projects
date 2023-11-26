c3926f9f135b7696ec9302020d905f5b2eca9f93
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-26 12:13:16 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -37,7 +37,7 @@ try:
     response_bytes = BytesIO(response.content)
     c1.image(Image.open(response_bytes),clamp=True,width=150)
 except:
-    c1.markdown(picked["images"].values[0])
+    c1.markdown("No Profile Picture")
 
 # rank
 rank = str(picked["rank"].values[0])
