b03808ca137a920a8189b255fcf2b55f00b0ea68
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-26 11:51:04 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -82,4 +82,5 @@ for i in picked["jutsu"].values[0]:
 # column 2
 c2.markdown("Debut: ")
 debut = str(str(picked["debut"].values[0]).split(",")[0]).split(":")[1]
+debut = debut.replace("'","")
 c2.markdown(debut)
