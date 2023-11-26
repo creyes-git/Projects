17ce7d471ceeb4a58ef9ccd04e8d33f2c5ac4db5
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-26 13:14:16 -0500

    Update 🥷Characters.py

--- Naruto_app/pages/🥷Characters.py
+++ Naruto_app/pages/🥷Characters.py
@@ -101,6 +101,6 @@ c2.header("Family: ")
 family = str(str(picked["family"].values[0]).replace("{","")).replace("}","")
 lista = list(family.split(","))
 for i in lista:
-    i.replace("'","")
+    i.replace("'"," ")
     i.capitalize()
     c2.markdown(i)
