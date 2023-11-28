39a148d9d6e689c315e8b0d4750918b2a7351622
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-28 18:22:58 -0500

    Update 📖Home.py

--- Naruto_app/📖Home.py
+++ Naruto_app/📖Home.py
@@ -6,27 +6,28 @@ st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
 #Introduction
 c1, c2 , c3, c4, c5 = st.columns(5)
+
 c1.image(Image.open("Naruto_app/images/wp2725528-naruto-kakashi-wallpaper.png"), width=150, clamp=True)
 c2.image(Image.open("Naruto_app/images/wp10367350-minato-minimalist-wallpapers.jpg"), width=150, clamp=True)
 c3.image(Image.open("Naruto_app/images/Landing Page.png"), width=150, clamp=True)
 c4.image(Image.open("Naruto_app/images/wp10926937-minimalist-4k-naruto-wallpapers.png"), width=150, clamp=True)
 c5.image(Image.open("Naruto_app/images/wp5129198-anime-lambang-uciha-wallpapers.jpg"), width=150, clamp=True)
 
-st.title("About Naruto")
+st.title("About Naruto:")
 st.markdown('''NARUTO is a manga series created by Masashi Kishimoto that was serialized in Weekly ShonenJump. 
 Met with popularity followingits initial publication in 1999, the series went on to receive an anime adaptation
 that commenced broadcasting in 2002. The manga concluded in 2014 with a total of 700 chapters. The total number 
 of print copies sold globally exceeds 250 million, and even now NARUTO events, games, merchandise,and more 
 continue to be created and released for fans to enjoy a testament to the series’ enduring influence and popularityaround the world.''')                
 
-st.title("Synopsis")
+st.header("Synopsis:")
 st.markdown('''Twelve years before the start of the series, the ninja village of Konohagakure was attacked by the Nine-Tailed Demon Fox.
 The village was only saved from complete destruction by the actions of Konoha's leader, the Fourth Hokage, who sacrificed his life to 
 seal the Nine-Tails into a newborn: Naruto Uzumaki. Orphaned by the attack, Naruto grew up shunned by most of the villagers, who hated 
 and feared him because of the Nine-Tails trapped within him. Unaware of the monster he contained, Naruto longed for acknowledgment, 
 and so vowed to one day become the greatest Hokage the village had ever seen.''')
 
-st.title("Details of Naruto MANGA")
+st.header("MANGA Insight's:")
 st.markdown("- Copies in circulation: 250 million copies in 47 countries and regions")
 st.markdown("- Tankōbon: 72 tankōbon released in Japan")
 st.markdown("- Volumes: 72 volumes")
@@ -34,7 +35,7 @@ st.markdown("- Naruto is one of the best-selling manga series in history. In Jap
 st.markdown("- Naruto is also a popular anime series. On the MyAnimeList website, Naruto is ranked 7th, behind newer anime like My Hero Academia, One Punch Man, and Attack on Titan.")
 
 #chart
-st.title("Top 10 Manga Series by Sales")
+st.header("Top 10 Manga's by Sales")
 df = pd.read_csv("Naruto_app/data_anime.csv")
 df.sort_values(by = "Approximate sales", ascending = False, inplace = True)
-st.bar_chart(data = df.head(10), x = "Manga series", y = "Approximate sales")
+st.bar_chart(data = df.head(10), x = "Manga series", y = "Approximate sales", use_container_width = True, color="orange")
