d7fbfbe5cf6280102be41cf23bb2fb98a05d3d2d
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-29 16:11:03 -0500

    Update 🩸Akatsuki.py

--- Naruto_app/pages/🩸Akatsuki.py
+++ Naruto_app/pages/🩸Akatsuki.py
@@ -2,4 +2,6 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Naruverse", page_icon="🍥", initial_sidebar_state= "expanded")
+st.set_page_config(page_title="Naruverse", page_icon="🍥")
+
+st.image(Image.open("Naruto_app//images//aka.png"), use_column_width= True, clamp=True)
