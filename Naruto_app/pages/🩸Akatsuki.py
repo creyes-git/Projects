7a7c53d503212a6e56350c9440f5d83a5cc77230
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-11-29 16:40:38 -0500

    Update 🩸Akatsuki.py

--- Naruto_app/pages/🩸Akatsuki.py
+++ Naruto_app/pages/🩸Akatsuki.py
@@ -4,4 +4,16 @@ from PIL import Image
 
 st.set_page_config(page_title="Naruverse", page_icon="🍥")
 
+st.title("Akatsuki History")
 st.image(Image.open("Naruto_app//images//aka.png"), use_column_width= True, clamp=True)
+
+st.markdown("The initial concept of Akatsuki was devised by Black Zetsu as part of its plan to revive its mother, though it pretended the idea came from Madara Uchiha.")
+st.markdown("Akatsuki was originally founded by Yahiko during the Third Shinobi World War alongside his childhood friends, Nagato and Konan. As citizens of Amegakure who had been orphaned by the Second Shinobi World War, the three created Akatsuki as a way to bring peace to their home country, which too often got caught in the crossfire of the Five Great Shinobi Countries' many conflicts. In time, the three were approached by Tobi, who introduced himself as Madara Uchiha. Tobi claimed Nagato was the reincarnation of the Sage of Six Paths and offered to teach him to control the power of his Rinnegan.")
+st.markdown("With Yahiko's dying breath he entrusted the pursuit of peace to Nagato. Nagato, grieved by Yahiko's death, came to believe that the world would never willingly consent to peace and that Akatsuki's previously pacifist approaches to achieving that goal were therefore futile. He concluded that the only way the world would turn away from its constant warfare was to experience such catastrophic death and destruction that it could never again stomach the idea of conflict. Intending to be the one to show these horrors to the world, Nagato began using the alias Pain and formed the Six Paths of Pain, into which he integrated Yahiko's corpse so that he could symbolically continue leading Akatsuki. Nagato sought out Obito, under the guise of Madara Uchiha, and accepted his earlier offer of support, which he provided in secret.")
+st.markdown("After the Third Shinobi World War, Akatsuki began recruiting S-rank missing-nin; mostly through strong-arming them and promising protection to carry out their respective goals. Orochimaru and Itachi joined for their own reasons, while Kisame and Zetsu joined to secretly ensure the Eye of the Moon Plan. Once Akatsuki recruited its target of ten members, Akatsuki began following a three-step process to achieve this stated objective:")
+st.markdown("1. Obtain enough money to support their organisation.")
+st.markdown("2. Provide mercenary services at well under the competitive rate, using the money gathered in the first step to offset costs. This will make countries increasingly dependent on Akatsuki's services, corner the entire market for shinobi missions, and eventually put all of the shinobi villages out of business because they can't compete with Akatsuki's prices. By capturing and strategically deploying the tailed beasts, Akatsuki could quickly start and then quell wars, thereby speeding up the process.")
+st.markdown("3. Once all other major shinobi forces have dissolved, Akatsuki will be able to quickly conquer all the countries of the world.")
+
+st.image(Image.open("Naruto_app//images//members.png"), use_column_width= True, clamp=True)
+st.markdown("The early years of Nagato's Akatsuki were very successful, as the hidden villages increasingly hired Akatsuki to fight in wars and perform assassinations that the villages themselves were unwilling to get involved in during times of peace. ")
