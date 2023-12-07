c3a384ad02e1f7f9fd36eaf465bcc131feae4073
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-06 21:46:06 -0500

    Update home.py

--- Cinema/home.py
+++ Cinema/home.py
@@ -4,12 +4,9 @@ import json
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Cinema", page_icon="🎥",layout= "wide")
+st.set_page_config(page_title="Cinema", page_icon="🎬",layout= "wide")
 
-st.sidebar.title("I'm your movie partner")
-
-st.title("Hello")
-
+st.title("I'm your movie recomendator 🤖")
 c1,c2,c3,c4,c5 = st.columns(5)
-c1.title("Trending:")
+c1.header(f"Trending today:")
 c2.image(Image.open("Cinema/images/1.png"))
