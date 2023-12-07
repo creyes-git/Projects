63eb938eecbf5e89e7f15ade4dde3f92c3db17f8
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-06 21:54:13 -0500

    Update home.py

--- Cinema/home.py
+++ Cinema/home.py
@@ -7,7 +7,7 @@ import emoji
 
 st.set_page_config(page_title="Cinema", page_icon="🎬",layout= "wide")
 
-st.title(f"I'm your movie recomendator {emoji.emojize(':robot:')}")
+st.title(f"I'm your movie recomendator 🤖")
 c1,c2,c3,c4,c5 = st.columns(5)
 c1.header(f"Trending today:")
 c2.image(Image.open("Cinema/images/1.png"))
