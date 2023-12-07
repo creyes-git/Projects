1c3c35e3847668f295a9a70e6f073885191dc74e
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-06 20:55:26 -0500

    Update home.py

--- Cinema/home.py
+++ Cinema/home.py
@@ -3,8 +3,7 @@ import requests
 import json
 import streamlit as st
 from PIL import Image
-from my_module import icon_calendar as icon
+
 
 st.set_page_config(page_title="Cinema", page_icon="🎬",layout= "wide")
-
 
