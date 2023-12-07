31e4d8779348c0f2323f201e4ab817d4cac98721
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-06 21:51:56 -0500

    Update home.py

--- Cinema/home.py
+++ Cinema/home.py
@@ -3,10 +3,11 @@ import requests
 import json
 import streamlit as st
 from PIL import Image
+import emoji
 
 st.set_page_config(page_title="Cinema", page_icon="🎬",layout= "wide")
 
-st.title("I'm your movie recomendator 🤖")
+st.title(f"I'm your movie recomendator {emoji.emojize(':robot:')}")
 c1,c2,c3,c4,c5 = st.columns(5)
 c1.header(f"Trending today:")
 c2.image(Image.open("Cinema/images/1.png"))
