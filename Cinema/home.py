57cc9f368a1f16f8451a9497f69b956ec981176a
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-06 20:56:14 -0500

    Update home.py

--- Cinema/home.py
+++ Cinema/home.py
@@ -3,7 +3,20 @@ import requests
 import json
 import streamlit as st
 from PIL import Image
-
+from my_module import icon_calendar as icon
 
 st.set_page_config(page_title="Cinema", page_icon="🎬",layout= "wide")
 
+url = "https://api.themoviedb.org/3/trending/movie/week?language=en-US"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjJlNDk5MTgxN2NmZDFjODM4MWU0ZTMzMGNhYWE1NiIsInN1YiI6IjY1NzExMTg4ZTRjOWViMDBmZGUwNWYwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7nxsSCK52vWr4esGTNBgCG04SwJKbrHdNNUe2ieP-hI"
+}
+
+response = requests.get(url, headers=headers)
+df = pd.DataFrame(response.json()["results"])
+
+st.sidebar.title("I'm your movie partner")
+
+st.title(f"Trending today: {icon()}")
