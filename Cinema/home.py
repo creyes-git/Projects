1032062b77b67d41e5ecfc153e7e411867d1df20
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-06 21:39:42 -0500

    Update home.py

--- Cinema/home.py
+++ Cinema/home.py
@@ -8,6 +8,6 @@ st.set_page_config(page_title="Cinema", page_icon="🎥",layout= "wide")
 
 st.sidebar.title("I'm your movie partner")
 
-c1,c2,c3,c4 = st.columns(4)
-c1.title(f"Trending today:")
+c1,c2,c3,c4,c5 = st.columns(5)
+c1.title("Trending:")
 c2.image(Image.open("Cinema/images/1.png"))
