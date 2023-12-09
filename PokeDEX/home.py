ef16c2e2f28bed39c9d83e34798e3b3077a58475
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-09 09:56:31 -0500

    Update home.py

--- PokeDEX/home.py
+++ PokeDEX/home.py
@@ -28,10 +28,8 @@ def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 
-
 lottie_pokeball = load_lottiefile("PokeDEX/pokeball.json")  # replace link to local lottie file
-
-st_lottie(lottie_pokeball)
+st.sidebar(st_lottie(lottie_pokeball, height = 200, quality = "high",size = "small"))
 
 
 # sidebar for searching Pokemon
