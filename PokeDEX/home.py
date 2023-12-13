33e6e4eb1b0074400642a2017ba3ff2b52047f7f
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-12 19:50:59 -0500

    Update home.py

--- PokeDEX/home.py
+++ PokeDEX/home.py
@@ -249,10 +249,7 @@ def display_similars(match):
 	# store all similar Pokemon with their stats in df
 	similar_pokemons_df = df_stats_all.loc[similar_pokemons]
 	
-	st.empty()
-	st.empty()
-	st.empty()	
-	st.empty()
+	st.write("\n")
  
 	col1, col2 = st.columns(2)
 	col1.subheader('Others Similar Pokemons:')
