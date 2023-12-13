22752a89421ae10a85a0c3475c75144d6eb00ed4
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-13 17:46:11 -0500

    Update home.py

--- PokeDEX/home.py
+++ PokeDEX/home.py
@@ -260,11 +260,11 @@ def display_similars(match):
                 #color_discrete_sequence = px.colors.sequential.Sunset_r ,line_shape="spline", range_r = [0, 200])
 				#fig.update_traces(fill="toself", mode="lines", line_shape="spline")
 				
-				fig = px.bar(row[1], y = row[1].index, x = name, template = "plotly_dark", range_color=[0, 250], color_continuous_scale = px.colors.sequential.Agsunset)
+				fig = px.bar(row[1], y = row[1].index, x = name, template = "plotly_dark", range_color=[0, 250], color_discrete_sequence=px.colors.qualitative.D3, barmode="group", opacity=0.8)
 
 				fig.update_layout(
 					xaxis_title="Value",
-					yaxis_title="Basic Stats")
+					yaxis_title="Stats")
     			
 				col2.plotly_chart(fig)
 		except:
