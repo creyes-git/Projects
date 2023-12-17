cdb5793c25fdfa0fc4fdd28a3eecc634e5c2abe2
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-17 12:28:02 -0500

    Update app.py

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -18,14 +18,14 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# getting the api key from a txt file
-with open("C:\\Users\\Carlos Reyes\\Desktop\\rentcast_api_key.txt", "r") as f: 
-    api_key = f.read()
+#getting the current date
+current_month_year = str(date.today()).split("-")[1] + "-" + str(date.today()).split("-")[0]
+    
 
-def get_rentcast_data_of_month(api_key):
-    #getting the current date
-    current_month_year = str(date.today()).split("-")[1] + "-" + str(date.today()).split("-")[0]
-
+def get_data_and_path(api_key):
+    headers = {"accept": "application/json",
+            "X-Api-Key": api_key}
+    
     list_calls = ["https://api.rentcast.io/v1/listings/sale?state=GA&propertyType=Condo&bedrooms=1&status=Active&limit=500",
                 "https://api.rentcast.io/v1/listings/sale?state=GA&propertyType=Condo&bedrooms=2&status=Active&limit=500",
                 "https://api.rentcast.io/v1/listings/sale?state=GA&propertyType=Condo&bedrooms=3&status=Active&limit=500",
@@ -48,21 +48,36 @@ def get_rentcast_data_of_month(api_key):
                 "https://api.rentcast.io/v1/listings/sale?city=Atlanta&state=GA&propertyType=Condo&bedrooms=2&status=Active&limit=500",
                 "https://api.rentcast.io/v1/listings/sale?city=Atlanta&state=GA&propertyType=Condo&bedrooms=3&status=Active&limit=500"]
 
-    headers = {"accept": "application/json",
-            "X-Api-Key": api_key}
-
+    # checking if the csv file of the current month already exists
     if not os.path.exists(f"Looking_My_Home/rentcast_data_{current_month_year}.csv"):
-        #creating an empty dataframe
         df = pd.DataFrame()
 
         for i in list_calls:
-            #making the call in each url of the list
+            # calling the api and concatinating in DataFrame
             response = requests.get(i, headers=headers).json()
-                
-            #concatenate the dataframes
             df = pd.concat([df, pd.DataFrame(response)], ignore_index=True)
-                
+                   
         #saving the dataframe to a csv file       
         df.to_csv(f"Looking_My_Home/rentcast_data_{current_month_year}.csv", index=False)
+        
+        return f"Looking_My_Home/rentcast_data_{current_month_year}.csv"
     else:
-        warnings.warn("File already exists. You can call the API only once a month")    
+        return f"Looking_My_Home/rentcast_data_{current_month_year}.csv"
+    
+    
+# getting the api key from a txt file
+with open("C:\\Users\\Carlos Reyes\\Desktop\\rentcast_api_key.txt", "r") as f: 
+    api_key = f.read()
+    
+df = pd.read_csv(get_data_and_path(api_key))
+df.dropna(how="all", inplace=True)
+df.drop_duplicates(inplace=True)
+
+st.table(df)
+    
+    
+    
+    
+        
+
+
