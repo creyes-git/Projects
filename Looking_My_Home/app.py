b0b818287a46b8f9c5ec19dc51c96010cf9565b4
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-21 17:07:06 -0500

    Update app.py

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -86,8 +86,9 @@ lottie_sidebar("Looking_My_Home/home1.json")
 def display_ga_map(dataframe):
     fig = ff.create_choropleth(dataframe)
     st.plotly_chart(fig)
-st.table(get_data_and_loaddf())    
-#display_ga_map(get_data_and_loaddf())
+
+
+display_ga_map(get_data_and_loaddf())
     
     
     
