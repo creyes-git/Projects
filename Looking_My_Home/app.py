561e85f047f1cb8baa8809c29587cdc3815cd4e7
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-22 19:21:52 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -84,7 +84,7 @@ def lottie_sidebar(path):
 lottie_sidebar("Looking_My_Home/home3.json")
 
 def display_ga_map(dataframe):
-    fig = px.scatter_mapbox(dataframe[["latitude", "longitude", "squareFootage", "price", "city", "state", "propertyType", "bedrooms", "bathrooms"]], lat="latitude", lon="longitude", size="squareFootage", zoom=10, color="price",
+    fig = px.scatter_mapbox(dataframe[["latitude", "longitude", "squareFootage", "price", "city", "state", "propertyType", "bedrooms", "bathrooms"]], lat="latitude", lon="longitude", zoom=10, color="price",
                             hover_name="city", hover_data=["propertyType", "bedrooms", "bathrooms", "price"])
     st.plotly_chart(fig)
 
