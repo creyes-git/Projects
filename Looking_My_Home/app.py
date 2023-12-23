d27bd6da6e2ce8688ba7ad2ad1966bd56a60122c
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-23 17:07:36 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -99,7 +99,7 @@ def display_ga_map(dataframe):
     fig.update_layout(
         mapbox=dict(
             center={"lat": 33, "lon": -83.23},
-            zoom=5,
+            zoom=5.3,
             style="carto-positron",
             ))
     
