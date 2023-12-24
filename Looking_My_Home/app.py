cbd25d6d2756717ff287084a952abf5fd200dc77
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-24 18:54:21 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -98,10 +98,11 @@ def display_ga_map(dataframe):
     # Update the layout of the scatter mapbox
     fig.update_layout(
         mapbox=dict(
-            px.set_mapbox_access_token(open(".mapbox_token").read()),
+            accesstoken= "mapbox://styles/mapbox/navigation-night-v1",
             center={"lat": 32.75, "lon": -83.23},
             zoom=5.25,
-            style="dark"))
+            style="carto-darkmatter",
+            ))
     
     st.plotly_chart(fig)
 
