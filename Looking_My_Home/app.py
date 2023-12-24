7eddf6202e0c32cf8c718d07ad8a092249871d9e
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-24 18:40:19 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -100,8 +100,7 @@ def display_ga_map(dataframe):
         mapbox=dict(
             center={"lat": 32.75, "lon": -83.23},
             zoom=5.25,
-            style="carto-darkmatter",
-            opacity=0.9))
+            style="carto-darkmatter"))
     
     st.plotly_chart(fig)
 
