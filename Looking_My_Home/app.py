baf8d0568d0ef21beb255b98c313e80b03d1dab0
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-24 18:57:35 +0000

    "white-bg"

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -100,7 +100,7 @@ def display_ga_map(dataframe):
         mapbox=dict(
             center={"lat": 32.75, "lon": -83.23},
             zoom=5.25,
-            style= "light"
+            style= "white-bg"
             ))
     
     st.plotly_chart(fig)
