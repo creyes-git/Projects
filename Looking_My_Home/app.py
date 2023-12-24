3f957a3b3d2041ca40edff79c75630130954a1b9
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-24 18:33:39 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -100,7 +100,7 @@ def display_ga_map(dataframe):
         mapbox=dict(
             center={"lat": 32.75, "lon": -83.23},
             zoom=5.25,
-            style="dark",
+            style="open-street-map",
             opacity=0.9)
     )
     
