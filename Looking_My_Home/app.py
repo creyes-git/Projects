8123cfd3dcbb4826ccb82c599f1210d368858316
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-25 22:34:15 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -114,21 +114,18 @@ def display_counties_ranking(dataframe):
 
 def display_choro_map(dataframe):
     with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-        gjson = json.load(response)
+        counties = json.load(response)
         
-    counties = dataframe[["county", "price"]]
-    counties = counties.groupby("county").mean().sort_values("price", ascending=False)
-    
-    fig = px.choropleth_mapbox(counties, geojson=gjson, color='price',
+        df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
+
+    fig = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='unemp',
                             color_continuous_scale="Viridis",
                             range_color=(0, 12),
-                            mapbox_style="white-bg",
-                            zoom=5.5, 
-                            center={"lat": 32.75, "lon": -83.23},
-                            opacity=0.9,
-                            #labels={'county':'price'},
-                            #hover_data={'county':True, 'price':True}
-                            )   
+                            mapbox_style="carto-positron",
+                            zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                            opacity=0.5,
+                            labels={'unemp':'unemployment rate'}
+                            )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
 
