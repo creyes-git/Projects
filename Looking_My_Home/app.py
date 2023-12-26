d03f98c9ea5731623ee0dd8663dcecff9109f540
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-26 00:10:04 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -126,7 +126,7 @@ def display_counties_ranking(dataframe):
 
 
 st.markdown("/n") 
-st.markdown("Georgia Property Market Dashboard" + ":home:") 
+st.markdown("Georgia Property Market Dashboard :graphic:")
 #st.markdown(f'<span class="icon type-text">Georgia Property Market Dashboard</span>', unsafe_allow_html=True)
 
 
