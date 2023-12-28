dfb64831a9942e339fd15b4feb9c646bb4ffc0ea
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-28 19:22:24 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -136,11 +136,11 @@ def call_sidebar():
         
         st.button("Search", type= "primary", on_click= None)
         
-        with st.expander('About', expanded=True):
+        with st.container():
             st.write('''- :red[**Data Source**]: [RentCast API](https://app.rentcast.io/app).
-                        :orange[**Made by**]: [**Carlos Reyes**](https://github.com/carlosreyes98)\
-                        :green[**Info & Sample**]: This app only shows Georgia state properties. The data is updated every month. 
-                        The sample of the total data is 5000 properties per month.''')
+                        - :yellow[**Info & Sample**]: This app only shows Georgia state properties. The data is updated every month.
+                        - :green[**Sample **]: The sample of the total data is 5000 properties per month.
+                        - :orange[**Made by**]: [**Carlos Reyes**](https://github.com/carlosreyes98)''')
 
 
 # Page Configuration and functions calling: #############################################################################################
