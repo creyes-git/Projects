0f7b6ff3b5581bccab80e44e7545cc0ecf304c43
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-28 19:02:50 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -128,8 +128,9 @@ def call_sidebar():
         st.selectbox("Property Type", df["propertyType"].unique(), index = None, placeholder= "Chose one")
         st.slider("Price Range: ", 0, max(df["price"]), (0, max(df["price"])), format="$%d")
         with st.container():
-            st.radio("Bedrooms: ", df["bedrooms"].sort_values().unique() , index=0)
-            st.radio("Bathrooms: ", df["bathrooms"].sort_values().unique() , index=0)
+            c1,c2 = st.columns(2)
+            c2.radio("Bedrooms: ", df["bedrooms"].sort_values().unique() , index=0)
+            c1.radio("Bathrooms: ", df["bathrooms"].sort_values().unique() , index=0)
         st.button("Search", type= "primary", on_click= None)
         
         with st.expander('About', expanded=True):
