db41d5b2980530eb99f019b3c65166a6f2258207
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2023-12-28 19:32:42 +0000

    git push

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -137,7 +137,7 @@ def call_sidebar():
         st.button("Search", type= "primary", on_click= None)
         
         # space
-        for i in range(10):
+        for i in range(3):
             st.markdown(" ")
         
         with st.container():
