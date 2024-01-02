a0b197ef73a2a902d7f94b838804fce9846c71c4
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-02 18:40:26 -0500

    Update app.py

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -169,7 +169,7 @@ def call_sidebar():
 def avg_stats(dataframe):
     
     avg_price = dataframe["price"].values.mean().astype(int).round(-3)
-    avg_size = dataframe["squareFootage"].values.mean()
+    avg_size = dataframe["squareFootage"].dropna(how="any").values.mean().astype(int)
     avg_beds = dataframe["bedrooms"].values.mean().astype(int)
     avg_baths = dataframe["bathrooms"].values.mean().astype(int)
     avg_days_market = dataframe["daysOnMarket"].values.mean().astype(int)
