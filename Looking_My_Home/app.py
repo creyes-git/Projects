4e5a6929a0d36b2dca35e1aa438752c79d2caca9
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-03 18:54:57 -0500

    Update app.py

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -175,7 +175,8 @@ def avg_price_size_bed_bath_mdays(dataframe):
     avg_days_market = dataframe["daysOnMarket"].values.mean().astype(int)
     
     with st.container():
-            st.markdown(f"# :rainbow[Avg Price: {str(avg_price)}$ ]")
+            st.markdown(f"# :rainbow[Average Statistics: ]")
+            st.markdown(f"# :rainbow[Avg Price: ]" + f" :red[{str(avg_price)}$ ]")
             st.markdown(f"# :rainbow[Avg Size: {str(avg_size)} sqft]")
             st.markdown(f"# :rainbow[Beds/Baths: {str(avg_beds)}/{str(avg_baths)}]")
             st.markdown(f"# :rainbow[Days on Market: {str(avg_days_market)}]")
