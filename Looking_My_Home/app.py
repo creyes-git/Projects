83171eaa18175f573cb7581ca09de0c0af4b0e69
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-02 19:56:11 -0500

    Update app.py

--- Looking_My_Home/app.py
+++ Looking_My_Home/app.py
@@ -176,10 +176,10 @@ def avg_price_size_bed_bath_mdays(dataframe):
     
     with st.container():
         col1, col2 = st.columns(2)
-        col1.markdown("Avg Price: ", f"# :red[{str(avg_price)}]")
-        col2.markdown("Avg Size: ", f"# :green[{str(avg_size)}]")
-        col1.markdown("Avg baths/beds: ", f"# :yellow[{str(avg_beds)}]")
-        col2.markdown("Avg Days on Market: ", f"# :orange[{str(avg_days_market)}]")
+        col1.markdown(f"# :red[{str(avg_price)}]")
+        col2.markdown(f"# :green[{str(avg_size)}]")
+        col1.markdown(f"# :yellow[{str(avg_beds)}]")
+        col2.markdown(f"# :orange[{str(avg_days_market)}]")
 
 # General info functions and stable charts:
 local_css('Looking_My_Home/style.css')
