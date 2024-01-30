e4bdaf1e8e600f938ff80189404f05c308cd9cbc
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-30 18:34:21 -0500

    Update app.py

--- CreditHub/app.py
+++ CreditHub/app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import pandas as pd
 import sqlite3 as sql
-import json
-import streamlit
 
 st.set_page_config(page_icon= "",page_title= "CreditHub", layout= "wide")
 
 connection = sql.connect("CreditHub\\DB\\Database.db")
 cursor = connection.cursor()
+
+
+Name = st.text_area("Enter the card name")
+Category = st.selectbox("Choose the category", options=  ["Travel", "Cash Back", "0% APR", "Airline", "Business", "Hotel", "Balance Transfer", "Rewards"])
+Welcome_Bonus = st.number_input("Enter the welcome bonus in Points", min_value= 0) 
+Annual_Fee = st.number_input("Enter the annual fee", min_value= 0)
+Recommended_Credit_Score = st.selectbox("Choose the recommended credit score range", options=["Bad (0-649)", "Fair (650-699)", "Good (700-749) ", "Excellent (750+)"])
+Issuer_Name = st.text_area("Enter the name of the issuer")
+Pros = st.text_area("Enter 3 PROS of the card separated by commas")
+Cons =  st.text_area("Enter 3 CONS of the card separated by commas")
+
