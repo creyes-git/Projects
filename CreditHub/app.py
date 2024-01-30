e80d8acad1e9932bfd3ce1aff20cef42f78f00b3
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-30 17:30:00 -0500

    w

--- CreditHub/app.py
+++ CreditHub/app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import sqlite3 as sql
+import json
+import streamlit
 
 st.set_page_config(page_icon= "",page_title= "CreditHub", layout= "wide")
 
 st.text("sdsd")
+
+connection = sql.connect("CreditHub\\DB\\Database.db")
+cursor = connection.cursor()
