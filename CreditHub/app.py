4ca20f2a456d953e75bb8262c756e8ebeade1615
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-31 18:06:07 -0500

    Update app.py

--- CreditHub/app.py
+++ CreditHub/app.py
@@ -17,10 +17,10 @@ Cons =  st.text_area("Enter 3 CONS of the card separated by commas")
 if st.button("Save"):
     connection = sql.connect("CreditHub\\DB\\My_Database.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS Cards(Issuer_Name TEXT, Name TEXT, Category TEXT, Welcome_Bonus INTEGER, Annual_Fee INTEGER, Recommended_Credit_Score TEXT, Pros TEXT, Cons TEXT)")
-    cursor.execute("INSERT INTO Cards VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (Issuer_Name, Name, Category, Welcome_Bonus, Annual_Fee, Recommended_Credit_Score, Pros, Cons))
+    cursor.execute("CREATE TABLE IF NOT EXISTS Card(Issuer_Name TEXT, Name TEXT, Category TEXT, Welcome_Bonus INTEGER, Annual_Fee INTEGER, Recommended_Credit_Score TEXT, Pros TEXT, Cons TEXT)")
+    cursor.execute("INSERT INTO Card VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (Issuer_Name, Name, Category, Welcome_Bonus, Annual_Fee, Recommended_Credit_Score, Pros, Cons))
     st.success("Your card has been submitted!")
-    st.text(cursor.execute("SELECT * FROM Cards").fetchall())
+    st.text(cursor.execute("SELECT * FROM Card").fetchall())
     connection.commit()
     connection.close()
 
