08cf64325934ea10027d5709879aa1284ce7d3cc
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-31 19:25:39 -0500

    w

--- CreditHub/app.py
+++ CreditHub/app.py
@@ -4,7 +4,7 @@ import sqlite3 as sql
 
 st.set_page_config(page_icon= "",page_title= "CreditHub", layout= "wide", initial_sidebar_state= "expanded")
 
-connection = sql.connect("CreditHub\\DB\\My_Database.db")
+connection = sql.connect("CreditHub\\DB\\My_DB.db")
 cursor = connection.cursor()
 
 
