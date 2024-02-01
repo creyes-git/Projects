0b8365b540fa5d089509a1d8ef33c5610d04b912
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-31 19:07:08 -0500

    w

--- CreditHub/app.py
+++ CreditHub/app.py
@@ -14,19 +14,15 @@ Issuer_Name = st.text_area("Enter the name of the issuer")
 Pros = st.text_area("Enter 3 PROS of the card separated by commas")
 Cons =  st.text_area("Enter 3 CONS of the card separated by commas")
 
+count = 0
 if st.button("Save"):
+   count += 1
+
+if count % 2 != 0:
     connection = sql.connect("CreditHub\\DB\\My_Database.db")
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS Card(Issuer_Name TEXT, Name TEXT, Category TEXT, Welcome_Bonus INTEGER, Annual_Fee INTEGER, Recommended_Credit_Score TEXT, Pros TEXT, Cons TEXT)")
     cursor.execute("INSERT INTO Card VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (Issuer_Name, Name, Category, Welcome_Bonus, Annual_Fee, Recommended_Credit_Score, Pros, Cons))
     st.success("Your card has been submitted!")
-    st.text(cursor.execute("SELECT * FROM Card").fetchall())
-    connection.commit()
-    connection.close()
-
-if st.button("Show all cards"):
-    connection = sql.connect("CreditHub\\DB\\My_Database.db")
-    cursor = connection.cursor()
-    st.table(cursor.execute("SELECT * FROM Card").fetchall())
     connection.commit()
     connection.close()
