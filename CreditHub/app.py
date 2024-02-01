998469f25df9c1492fc0a5d38a4e4437ee512ad8
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-01-31 19:22:16 -0500

    w

--- CreditHub/app.py
+++ CreditHub/app.py
@@ -7,22 +7,32 @@ st.set_page_config(page_icon= "",page_title= "CreditHub", layout= "wide", initia
 connection = sql.connect("CreditHub\\DB\\My_Database.db")
 cursor = connection.cursor()
 
+
 Name = st.text_area("Enter the card name")
 Category = st.selectbox("Choose the category", options=  ["Travel", "Cash Back", "0% APR", "Airline", "Business", "Hotel", "Balance Transfer", "Rewards"])
+Rewards_rate = st.text_input("Enter the rewards rate")
 Welcome_Bonus = st.number_input("Enter the welcome bonus in Points", min_value= 0) 
 Annual_Fee = st.number_input("Enter the annual fee", min_value= 0)
 Recommended_Credit_Score = st.selectbox("Choose the recommended credit score range", options=["Bad (0-649)", "Fair (650-699)", "Good (700-749) ", "Excellent (750+)"])
 Issuer_Name = st.text_area("Enter the name of the issuer")
 Pros = st.text_area("Enter 3 PROS of the card separated by commas")
 Cons =  st.text_area("Enter 3 CONS of the card separated by commas")
+Image_URL = st.text_input("Enter the URL of the card image")
 
 
 if st.button("Save"):
     cursor.execute("CREATE TABLE IF NOT EXISTS Cards(Issuer_Name TEXT, Name TEXT, Category TEXT, Welcome_Bonus INTEGER, Annual_Fee INTEGER, Recommended_Credit_Score TEXT, Pros TEXT, Cons TEXT)")
-    cursor.execute("INSERT INTO Cards VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (Issuer_Name, Name, Category, Welcome_Bonus, Annual_Fee, Recommended_Credit_Score, Pros, Cons))
+    cursor.execute("INSERT INTO Cards VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (Issuer_Name, Name, Category, Rewards_rate, Welcome_Bonus, Annual_Fee, Recommended_Credit_Score, Pros, Cons, Image_URL))
     st.success("Your card has been submitted!")
+    connection.commit()
+    connection.close()
+
+if st.button("View Cards"):
+    st.dataframe(cursor.execute("SELECT * FROM Cards"))
+    connection.commit()
+    connection.close()
+
+# df = pd.read_sql_query("SELECT * FROM Cards", connection)
 
 
-
-connection.commit()
 
