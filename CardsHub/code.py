a7265f827d564051dd1ece50eaf3afd998bacaeb
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-02-08 19:26:35 -0500

    w

--- CardsHub/code.py
+++ CardsHub/code.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import pandas as pd
 import sqlite3 as sql
+import streamlit_lottie as st_lottie
+import json
 
 #setting credit card page icon and title
 st.set_page_config(page_icon= "💳",page_title= "CardsHub", layout= "wide", initial_sidebar_state= "expanded")
 
-connection = sql.connect("CardsHub\\Cards.db")
-cursor = connection.cursor()
+# load lottie animation
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+st_lottie(load_lottiefile("CardsHub\\images\\lottie1.json"), height = 60, quality = "high")
 
 st.title("Fill card details on the form below:")
 
+# card form
 with st.form(key="card_form", clear_on_submit= True):
     Issuer_Name = st.selectbox("Select the issuer", options=  ["Discover", "Chase", "Bank of America","Wells Fargo","Citi", "Capital One", "Credit One Bank", "American Express", "VISA", "Mastercard"])
     Name = st.text_area("Enter the card name")
@@ -22,23 +29,4 @@ with st.form(key="card_form", clear_on_submit= True):
     Cons =  st.text_area("Enter 3 CONS of the card separated by line breaks")
     Image_URL = st.text_input("Enter the URL of the card image")
     
-    boton = st.form_submit_button("Submit Card")
-    
-
-    if boton:
-        cursor.execute("CREATE TABLE IF NOT EXISTS Cards(Issuer_Name TEXT, Name TEXT, Rewards_rate TEXT, Category TEXT, Welcome_Bonus INTEGER, Annual_Fee INTEGER, Recommended_Credit_Score TEXT, Pros TEXT, Cons TEXT, Image_URL TEXT)")
-        cursor.execute("INSERT INTO Cards VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (Issuer_Name, Name, Category, Rewards_rate, Welcome_Bonus, Annual_Fee, Recommended_Credit_Score, Pros, Cons, Image_URL))
-        st.success("Your card has been submitted!")
-        connection.commit()
-        connection.close()
-
-if st.button("View Cards"):
-    st.dataframe(cursor.execute("SELECT * FROM Cards"))
-    connection.commit()
-    connection.close()
-    
-if st.button("Delete Cards"):
-    cursor.execute("DELETE FROM Cards")
-    st.success("All cards have been deleted!")
-    connection.commit()
-    connection.close()
+    #if st.form_submit_button("Submit Card"):
