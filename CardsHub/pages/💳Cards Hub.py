27aae7fbe543d7b0d2939964afd4a1d03c910c80
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-04-06 17:29:04 +0000

    git push

---
+++ CardsHub/pages/💳Cards Hub.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import streamlit_lottie as st_lottie
+import sqlite3 as sql
+import pandas as pd
+import plotly.express as px
+from PIL import Image
+import requests
+import json
+
+
+connection = sql.connect("Cards.db")
+cursor = connection.cursor()
+
+# functions:
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+c1, c2,c3 = st.columns(3)
+with c2:
+    st_lottie(load_lottiefile("images/card_hand.json"), height = 111, quality = "high")
