df74e717d4c2ec7671d4523b876d1afb4d8be859
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-04-27 20:59:45 +0000

    git push

--- Anime_recomendation/Anime.py
+++ Anime_recomendation/Anime.py
@@ -4,55 +4,45 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 
-# check API details bellow:
+st.set_page_config(page_title="Anime Explorer", page_icon="✨", layout="centered", initial_sidebar_state="expanded")
+
+# API:
 url = "https://anime-db.p.rapidapi.com/anime"
 headers = {"X-RapidAPI-Key": "8af8022ddcmsh06e9119b8cc13f3p1a0e1fjsn8138c83b2c3b",
             "X-RapidAPI-Host": "anime-db.p.rapidapi.com"}
 query = {"page": "1", "size": "10000"}
+
 list_genders = ["Fantasy", "Drama", "Action", "Award Winning", "Slice of Life", "Suspense", "Horror", "Ecchi", "Avant Garde", "Erotica"
                 "Comedy", "Hentai", "Boys Love", "Gourmet", "Girls Love", "Romance", "Adventure", "Mystery", "Supernatural", "Sci-Fi"]
 
 
-# Streamlit APP:
-picture = Image.open("Anime_recomendation/kimetsu.jpg")
-st.image(picture, caption="WELCOME TO THE ANIME EXPLORER :)")
+st.title(":orange[**Welcome to the Anime Explorer**]")
+st.image(Image.open("kimetsu.jpg"), caption="Fill your preferences and get anime recommendations")
+st.write("---")
 
-st.title("Fill your app preferences")
-
-st.text("                                                                                                                              ")
-st.text("                                                                                                                              ")
-st.text("                                                                                                                              ")
-
-genders = st.selectbox('Select the gender that you want to see: ', list_genders)
-
-type = st.radio(" Movie or TV Serie?: ", ("Movie", "TV"), index=0)
-
-
-if type == "TV":
-    key = False
-
-episodes = st.selectbox("How many episodes do you want to see: ", options= ["1-50", "51-100", "100-200", "200-1000"],disabled= key, placeholder= "Chose a range")
-
-status = st.radio(" Status: ", ("Finished Airing", "Not yet aired"), index=0)
-
-
-#search button:  
-button = st.button("Search", type= "primary")   
-
+# Form
+with st.form(key="search_form", clear_on_submit= True):
+    type = st.radio(" Movie or TV Serie?: ", ("Movie", "TV"), index=None)
+    if type == "Movie":
+        a = True
+    else:
+        a = False
+    genders = st.selectbox('Select the gender that you want to see: ', list_genders, index = None)
+    status = st.radio(" Status: ", ("Finished Airing", "Not yet aired"), index = None, disabled = a)
+    episodes = st.selectbox("How many episodes do you want to see: ", options= ["1-20", "21-50", "51-100", "100+"],disabled= a, index = None)
+    
+    button = st.form_submit_button(":orange[**Search**]")   
 
 
 if button:    
-    #Calling API:
+    #Calling API and converting to DataFrame:
     response = requests.get(url, headers=headers, params=query)
     df = pd.DataFrame(response.json()["data"])
-    
+
     df = df[df["type"] == type]
-    
     df = df[df["status"] == status]
+    df = df[df["genres"].apply(lambda x: genders in x)]    
     
-    df = df[df["genres"].apply(lambda x: genders in x)]
-    
-        
     if type == "TV":
         df = df[df["episodes"] <= int(str(episodes).split("-")[1])] 
         df = df[df["episodes"] >= int(str(episodes).split("-")[0])]
