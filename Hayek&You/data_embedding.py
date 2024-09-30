cb3fde1c0bd53bdb313de30176fc26b4888f588e
carlosreyes98 <134648583+carlosreyes98@users.noreply.github.com>
2024-09-30 14:09:43 +0000

    git push

--- Hayek&You/data_embedding.py
+++ Hayek&You/data_embedding.py
@@ -1,9 +1,20 @@
+import sqlite3
 from langchain_chroma import Chroma
 from langchain_voyageai import VoyageAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from dotenv import load_dotenv
+
 import os 
 
 load_dotenv()
 
 
+# Specify the path to the folder
+folder_path = r"/workspaces/Projects/Hayek&You/data"
+
+# Get a list of all files in the folder
+files_name = os.listdir(folder_path)
+
+# Print the names of all files
+for file in files_name:
+    print(file)
