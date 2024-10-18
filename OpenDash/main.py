b3e374a7787c5b6110b903fda27a5c0e09fe3003
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-10-18 19:51:52 +0000

    git push

--- OpenDash/main.py
+++ OpenDash/main.py
@@ -4,3 +4,13 @@ import streamlit as st
 
 st.set_page_config(page_title="OpenDash", page_icon=":stock:", layout = "centered", initial_sidebar_state = "expanded")
 
+
+with st.sidebar:
+    select_index = st.selectbox("Index", get_index_symbols().unique())
+    
+    show_chart = st.button("Show Chart", type = "primary")
+    
+    
+if show_chart:
+    st.line_chart(get_index_hprice(select_index), use_container_width = True)
+    
--- OpenDash/stock_funcs.py
+++ OpenDash/stock_funcs.py
@@ -1,6 +1,7 @@
 from openbb import obb
 import pandas as pd
 
+
 def get_index_symbols():
     """
     Get the list of available index symbols from Yahoo Finance.
@@ -10,8 +11,8 @@ def get_index_symbols():
     index_symbols : pandas.Series
         A pandas Series of index symbols available from Yahoo Finance.
     """
-    index_symbols = obb.index.available(provider = "yfinance").to_dataframe()["symbol"]
-    
+    index_symbols = obb.index.available(provider = "yfinance").to_dataframe()["name"]
+
     return index_symbols
 
 
@@ -29,6 +30,6 @@ def get_index_hprice(symbol):
     pd.DataFrame
         A DataFrame with the historical price of the index.
      """
-    index_price = obb.index.price.historical(symbol, provider = "yfinance").to_dataframe()
+    index_price = obb.index.price.historical(symbol, provider = "yfinance", start_date = "2014-01-01").to_dataframe()["close"]
     
     return index_price
