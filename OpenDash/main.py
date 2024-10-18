73b8ae066096bfe105089d9c7b6b8ebf16cb2b25
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-10-18 19:23:46 +0000

    git push

---
+++ OpenDash/main.py
@@ -0,0 +1,6 @@
+from stock_funcs import *
+import streamlit as st
+
+
+st.set_page_config(page_title="OpenDash", page_icon=":stock:", layout = "centered", initial_sidebar_state = "expanded")
+
---
+++ OpenDash/stock_funcs.py
@@ -0,0 +1,34 @@
+from openbb import obb
+import pandas as pd
+
+def get_index_symbols():
+    """
+    Get the list of available index symbols from Yahoo Finance.
+    
+    Returns
+    -------
+    index_symbols : pandas.Series
+        A pandas Series of index symbols available from Yahoo Finance.
+    """
+    index_symbols = obb.index.available(provider = "yfinance").to_dataframe()["symbol"]
+    
+    return index_symbols
+
+
+def get_index_hprice(symbol):
+    """
+    Get the historical price of a given index symbol.
+
+    Parameters
+    ----------
+    symbol : str
+        The symbol of the index.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with the historical price of the index.
+     """
+    index_price = obb.index.price.historical(symbol, provider = "yfinance").to_dataframe()
+    
+    return index_price
