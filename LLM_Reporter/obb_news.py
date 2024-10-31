90731cc3d4e56e0b24d3566a81c4d53c5d32b5e6
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-10-31 14:56:55 +0000

    git push

--- LLM_Reporter/obb_news.py
+++ LLM_Reporter/obb_news.py
@@ -1,3 +1,15 @@
 from openbb import obb
+from datetime import datetime, timedelta
+import pandas as pd
+from dotenv import load_dotenv
 
-obb.news()
+load_dotenv()
+
+last_week = (datetime.today() - timedelta(days=7)).strftime("%Y-%m-%d")
+
+
+news = obb.news.world(start_date = last_week, provider = "tiingo")
+
+df_news = news.to_dataframe()
+
+print(df_news)
