6a24f6013a13fc85a4c2ff07e19b3cae366081a6
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-11-22 15:49:38 +0000

    git push

--- Cashew_Board/main.py
+++ Cashew_Board/main.py
@@ -1,16 +1,16 @@
-import pandas as pd
+import pandas as pd 
+import plotly.graph_objects as go
+from datetime import datetime
 import os
-
 
 data_folder = r"/workspaces/Projects/Cashew_Board/data"
 file_on_folder = os.listdir(data_folder)[0]
-
 
 def android_to_hex(color_code):
     hex_code = "#{:06x}".format(color_code & 0xffffff)
     return hex_code
 
-
 df = pd.read_csv(fr"{data_folder}/{file_on_folder}", engine = "pyarrow", keep_default_na = False)
 df["color"] = df["color"].apply(android_to_hex)
 df['amount'] = df['amount'].abs() # Transform negative values to positive
+df["date"] = df["date"].dt.strftime("%Y-%m")
--- Cashew_Board/scripts/plotly_charts.py
+++ Cashew_Board/scripts/plotly_charts.py
@@ -4,17 +4,6 @@ import plotly.graph_objects as go
 
 
 def plot_pie_categories(df):
-    """This function takes a pandas DataFrame as argument and generates a pie chart based on the categorical distribution of the 'amount' column.
-    
-    Parameters
-    ----------
-    df : pandas DataFrame
-        The DataFrame should contain the columns 'category name', 'amount', and 'color'.
-    
-    Returns
-    -------
-    fig : plotly.graph_objs.Figure
-        The pie chart figure."""
     
     df = df[df["income"] == False] # Keeps only expenses
     df["category name"] = df.apply(lambda row: row["category name"] if row["subcategory name"] == "" else row["subcategory name"], axis = 1) # Keep subcategory if not empty
@@ -24,7 +13,7 @@ def plot_pie_categories(df):
     fig.add_trace(go.Pie(labels = df["category name"],
                          values = df["amount"],
                          hoverinfo = "value+percent",
-                         #hovertemplate = "<extra>$%{value}</extra> : %{percent}</extra>",
+                         hovertemplate = "$%{value} = %{percent} <extra></extra>",
                          hoverlabel = dict(font_size = 15),
                          textinfo = "label",
                          textposition = "inside",
@@ -34,4 +23,64 @@ def plot_pie_categories(df):
                          hole = 0.4,
                          marker = dict(colors = df["color"],)))
     
+    return fig
+
+
+def plot_hist_income_expense(df):
+    
+    df_income = df[df["income"] == True] # Keeps only incomes
+    df_expense = df[df["income"] == False] # Keeps only expenses
+    df_income = df_income[["amount", "date"]].groupby(["date"]).agg({"amount": "sum"}).reset_index() # Group by date and sum amount
+    df_expense = df_expense[["amount", "date"]].groupby(["date"]).agg({"amount": "sum"}).reset_index()
+    
+    fig = go.Figure(layout = go.Layout(height = 500, width = 1500)).update_layout(title = "Monthly Income-Expenses Evolution")
+    
+    fig.add_trace(go.Bar(x = df_income["date"],
+                         y = df_income["amount"],
+                         hoverinfo = "y",
+                         hovertemplate = "Income: $%{y} <extra></extra>",
+                         hoverlabel = dict(font_size = 15),
+                         showlegend = False,
+                         marker_color = "#4caf50")) # Income Trace
+    
+    fig.add_trace(go.Bar(x = df_expense["date"],
+                         y = df_expense["amount"],
+                         hoverinfo = "y",
+                         hovertemplate = "Expense: $%{y} <extra></extra>",
+                         hoverlabel = dict(font_size = 15),
+                         showlegend = False,
+                         marker_color = "#ff7043")) # Expense Trace
+    
+    fig.add_trace(go.Line(x = df_income["date"],
+                          y = df_income["amount"] - df_expense["amount"],
+                          hoverinfo = "y",
+                          hovertemplate = "Saving: $%{y} <extra></extra>",
+                          hoverlabel = dict(font_size = 15),
+                          showlegend = False,
+                          line_color = "#FFBE04")) # Saving Trace
+    
+    return fig
+
+
+def plot_saving_rate(df):
+    
+    df = df[["income", "amount"]].groupby(["income"]).agg({"amount": "sum"}).reset_index() # Group by type and sum amount
+    df["income"] = df["income"].replace({True: "Income", False: "Expense"})
+    
+    fig = go.Figure(layout = go.Layout(height = 500, width = 500))
+    
+    fig.add_trace(go.Pie(labels = df["income"],
+                         values = df["amount"],
+                         hovertemplate = "%{label}: $%{value} <extra></extra>",
+                         hoverlabel = dict(font_size = 15),
+                         showlegend = False,
+                         hole = 0.4,
+                         marker = dict(colors = ["#ff7043", "#4caf50"])))
+    
+    fig.add_annotation(x = 0.5, 
+                       y = 1.25,
+                       text = f"Savings Rate: {round(((df['amount'][1] - df['amount'][0]) / df['amount'][1]) * 100, 2)} %",
+                       showarrow = False, 
+                       font = dict(size = 16, color = 'black'))
+
     return fig
