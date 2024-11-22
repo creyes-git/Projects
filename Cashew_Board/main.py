9ee126b5656674c8c3766dc5146b2dddcff41f18
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-11-22 20:35:39 +0000

    git push

--- Cashew_Board/main.py
+++ Cashew_Board/main.py
@@ -13,14 +13,13 @@ def local_css(file_name):
 local_css('assets/style.css')
 
 
-data_folder = r"/workspaces/Projects/Cashew_Board/data"
-file_on_folder = os.listdir(data_folder)[0]
-
-
 def android_to_hex(color_code):
     hex_code = "#{:06x}".format(color_code & 0xffffff)
     return hex_code
 
+
+data_folder = r"/workspaces/Projects/Cashew_Board/data"
+file_on_folder = os.listdir(data_folder)[0]
 
 df = pd.read_csv(fr"{data_folder}/{file_on_folder}", engine = "pyarrow", keep_default_na = False)
 df["color"] = df["color"].apply(android_to_hex)
