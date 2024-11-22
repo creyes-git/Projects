16d777a29b8494dd1df12c7263a9821680cbfaac
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-11-22 13:53:33 +0000

    git push

--- Cashew_Board/scripts/plotly_charts.py
+++ Cashew_Board/scripts/plotly_charts.py
@@ -1,36 +1,21 @@
 import pandas as pd 
+import plotly.express as px
 import plotly.graph_objects as go
-#import webcolors
-import os
-
-
-data_folder = r"/workspaces/Projects/Cashew_Board/data"
-file_on_folder = os.listdir(data_folder)[0]
-
-
-def android_to_hex(color_code):
-    hex_code = "#{:06x}".format(color_code & 0xffffff)
-    return hex_code
-
-
-df = pd.read_csv(fr"{data_folder}/{file_on_folder}", engine = "pyarrow", keep_default_na = False)
-df["color"] = df["color"].apply(android_to_hex)
 
 
 def plot_pie_categories(df):
+    """This function takes a pandas DataFrame as argument and generates a pie chart based on the categorical distribution of the 'amount' column.
     
-    """Plot a pie chart of the categories of expenses in a given dataframe.
-
     Parameters
     ----------
-    df : pandas dataframe
-        The dataframe containing the expenses to be plotted.
-
+    df : pandas DataFrame
+        The DataFrame should contain the columns 'category name', 'amount', and 'color'.
+    
     Returns
     -------
-    fig : plotly.graph_objects.Figure
+    fig : plotly.graph_objs.Figure
         The pie chart figure."""
-        
+    
     df = df[df["income"] == False] # Keeps only expenses
     df['amount'] = df['amount'].abs() # Transform negative values to positive
     df["category name"] = df.apply(lambda row: row["category name"] if row["subcategory name"] == "" else row["subcategory name"], axis = 1) # Keep subcategory if not empty
@@ -39,12 +24,15 @@ def plot_pie_categories(df):
     fig = go.Figure(layout = go.Layout(height = 500, width = 500))
     fig.add_trace(go.Pie(labels = df["category name"],
                          values = df["amount"],
-                         hoverinfo = "label+value+percent",
-                         textinfo = "label+value",
+                         hoverinfo = "value+percent",
+                         #hovertemplate = "<extra>$%{value}</extra> : %{percent}</extra>",
+                         hoverlabel = dict(font_size = 15),
+                         textinfo = "label",
                          textposition = "inside",
+                         insidetextorientation = "radial",
+                         insidetextfont = dict(color = "black"),
                          showlegend = False,
+                         hole = 0.4,
                          marker = dict(colors = df["color"],)))
     
     return fig
-
-plot_pie_categories(df)
