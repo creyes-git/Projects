51e32bb86528f3cc006e84361af487ab22375f62
Carlos Reyes <134648583+carlosreyes98@users.noreply.github.com>
2024-11-22 20:19:35 +0000

    git push

--- Cashew_Board/main.py
+++ Cashew_Board/main.py
@@ -3,15 +3,24 @@ import streamlit as st
 import pandas as pd
 import os
 
+
 st.set_page_config(page_title = "Cashew Board", page_icon = ":moneybag:", layout = "wide", initial_sidebar_state = "expanded")
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+local_css('assets/style.css')
 
 
 data_folder = r"/workspaces/Projects/Cashew_Board/data"
 file_on_folder = os.listdir(data_folder)[0]
 
+
 def android_to_hex(color_code):
     hex_code = "#{:06x}".format(color_code & 0xffffff)
     return hex_code
+
 
 df = pd.read_csv(fr"{data_folder}/{file_on_folder}", engine = "pyarrow", keep_default_na = False)
 df["color"] = df["color"].apply(android_to_hex)
@@ -20,8 +29,7 @@ df['amount'] = df['amount'].abs() # Transform negative values to positive
 
 with st.sidebar:
     c1, c2 = st.columns(2)
-    c1.write("Welcome")
-    c1.write("Welcome to Cashew Board")
+    c1.markdown('<span class="icon type-red">Cashew</span>', unsafe_allow_html = True)
     c2.image("/workspaces/Projects/Cashew_Board/assets/icon/icon.png", width = 100, clamp = True)
     st.markdown("---")
     st.image("/workspaces/Projects/Cashew_Board/assets/images/empty.png", use_column_width = True)
