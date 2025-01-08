f27d1596bb73e77422a667a170343d062d2e91e9
Carlos Reyes <134648583+creyes-git@users.noreply.github.com>
2025-01-08 16:05:21 +0000

    git push

--- Cashew_Board/main.py
+++ Cashew_Board/main.py
@@ -16,17 +16,16 @@ with open("assets/style.css") as f:
 with st.sidebar:
     c1, c2 = st.columns(2)
     
-    c1.markdown('<span class="bubble type-beiche">Welcome</span>', unsafe_allow_html = True)
-    c1.markdown('<span class="bubble type-blue">TO</span>', unsafe_allow_html = True)
-    c1.markdown('<span class="bubble type-green">Cashew</span>', unsafe_allow_html = True)
-    c1.markdown('<span class="bubble type-red">Board</span>', unsafe_allow_html = True)
+    c2.markdown('<span class="bubble type-beiche">Welcome</span>', unsafe_allow_html = True)
+    c2.markdown('<span class="bubble type-blue">TO</span>', unsafe_allow_html = True)
+    c2.markdown('<span class="bubble type-green">Cashew</span>', unsafe_allow_html = True)
+    c2.markdown('<span class="bubble type-red">Board</span>', unsafe_allow_html = True)
 
-    c2.image("assets/images/icon.png", width = 115, clamp = True)
+    c1.image("assets/images/icon.png", width = 115, clamp = True)
     st.markdown("---")
-    #st.image("assets/images/empty.png", use_column_width = True)
     
     # READ CASH APP CSV TO DATAFRAME
-    cashew_csv_file = st.file_uploader(label = "Upload Your Cashew CSV Data Here", type = ["csv"], accept_multiple_files = False, help = "Go to Cashew App > Settings > Export Data > Export CSV")
+    cashew_csv_file = st.file_uploader(label = "Your Cashew CSV Data Here", type = ["csv"], accept_multiple_files = False, help = "Go to Cashew App > Settings > Export Data > Export CSV")
     
     if cashew_csv_file:
         df = pd.read_csv(cashew_csv_file, engine = "pyarrow", keep_default_na = False)
@@ -35,10 +34,10 @@ with st.sidebar:
         df["date"] = pd.to_datetime(df["date"].dt.strftime("%m-%d-%Y")) # Format date to keep only month-day-year
     
     else:
-        df = None
+        df = pd.DataFrame()
 
 
-if df:
+if df.empty == False:
     # MAIN PAGE------------------------------------------------------------------------------------------------------------------------------------------------------------
     with st.container(): # Main Page Header Container
         c1, c2, c3, c4 = st.columns(4)
@@ -63,5 +62,10 @@ if df:
         c2.plotly_chart(plot_pie_categories(df), use_container_width = True)
         
 else:
+    c1, c2, c3, c4, c5 = st.columns(5)
+    c2.title("_Upload Your_")
+    c3.title(":orange[CASHEW]:sunglasses:")
+    c4.title("_CSV To Start_")
+    c2.write("\n")
     c1, c2, c3 = st.columns(3)
-    c2.image("assets/images/no-search-filter.png", use_container_width = True, clamp = True)
+    c2.image("assets/images/no-search.png")
